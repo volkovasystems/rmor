ff11be748c7f082278b06689d21119d2e247c584
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-16 14:19:18 +0800

    Updated server/client test.

--- rmor-test.js
+++ rmor-test.js
@@ -1,7 +1,8 @@
 const assert = require( "assert" );
 const rmor = require( "./rmor.js" );
 
-assert.deepEqual( rmor( { "hello": "world" } ), { "hello": "world" } );
-assert.deepEqual( rmor( global ), { } );
+assert.deepEqual( rmor( { "hello": "world" } ), { "hello": "world" }, "should be deeply equal" );
+
+assert.equal( typeof rmor( global ), "object", "should return 'object'" );
 
 console.log( "ok" );
