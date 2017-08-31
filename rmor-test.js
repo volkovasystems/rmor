6721aecf3fd4ac253f395cb0648b6c533d9faeec
Vinse Vinalon <vinsevinalon@gmail.com>
2017-08-31 13:42:43 +0800

    Update client/server unit test.

--- rmor-test.js
+++
@@ -1,8 +0,0 @@
-const assert = require( "assert" );
-const rmor = require( "./rmor.js" );
-
-assert.equal( typeof rmor( global ) == "object", true, "should be of object data type" );
-
-assert.deepEqual( rmor( { "hello": "world" } ), { "hello": "world" }, "should be equal to { 'hello': 'world' }" );
-
-console.log( "ok" );
